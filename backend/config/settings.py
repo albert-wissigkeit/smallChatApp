95d576b0a53eafb52ba59348c24ffb4be42cd584
albert-wissigkeit <wissigkeit@outlook.de>
2025-01-11 20:43:27 +0100

    change cors headers

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chat_app'
+    'chat_app',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,19 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+  'http://127.0.0.1:5500',
+  'http://localhost:5500',
+]
+
+CORS_ALLOWED_ORIGINS = [
+  'http://127.0.0.1:5500',
+  'http://localhost:5500',
+  'http://127.0.0.1:4200',  # Angular-Frontend
+  'http://localhost:4200', 
 ]
 
 ROOT_URLCONF = 'config.urls'
