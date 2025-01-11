1da8affae67c9f9f40a45e02b7712bed5075f03a
albert-wissigkeit <wissigkeit@outlook.de>
2025-01-11 19:26:46 +0100

    added chat_view

--- backend/chat_app/models.py
+++ backend/chat_app/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Chat(models.Model): 
     name = models.CharField(max_length=255, blank=False)
     message = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)   # muss noch auf readonly im serializer gesetzt werden oder im admin interface, damit es nicht über das frontend geändert werden kann
+    created_at = models.DateTimeField(auto_now_add=True)
--- backend/chat_app/views.py
+++ backend/chat_app/views.py
@@ -1,3 +1,16 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
+from chat_app.models import Chat
+
 # Create your views here.
+def chat_view(request):
+    if request.method == "GET":
+        chat_data = Chat.objects.all().values('name', 'message', 'created_at')  
+        # ruft alle Chat-Objekte ab und verwendet `values`, um nur die Felder 'name', 'message' und 'created_at' auszuwählen. 
+        # `values()` gibt ein QuerySet zurück, das eine Liste von Dictionaries enthält.
+        
+        return JsonResponse(list(chat_data), safe=False)  
+        # Mit `list()` wird das QuerySet `chat_data` in eine reguläre Python-Liste umgewandelt, die JSON-kompatibel ist.
+        # Wenn man eine Liste anstatt eines Dictionaries zurückgibt, muss `safe=False` gesetzt werden, 
+        # da `JsonResponse` standardmäßig nur ein Dictionary akzeptiert.
