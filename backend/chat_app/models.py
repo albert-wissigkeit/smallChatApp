8ee589b14ba931bb606da477a34a1bc05de91ab0
albert-wissigkeit <wissigkeit@outlook.de>
2025-01-11 19:03:07 +0100

    added Chat Model

--- backend/chat_app/models.py
+++ backend/chat_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Chat(models.Model): 
+    name = models.CharField(max_length=255, blank=False)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)   # muss noch auf readonly im serializer gesetzt werden oder im admin interface, damit es nicht über das frontend geändert werden kann
